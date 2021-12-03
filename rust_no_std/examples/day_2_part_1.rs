b39ae9cfba171e87123b13c9bf2ae926626fa154
Dylan Frankland <dfrankland@users.noreply.github.com>
2021-12-03 07:29:02 +0000

    day 2 - rust_no_std

---
+++ rust_no_std/examples/day_2_part_1.rs
@@ -0,0 +1,41 @@
+#![no_std]
+#![no_main]
+
+#[allow(unused_imports)]
+use rust_no_std::my_panic;
+use libc_print::libc_println;
+
+#[no_mangle]
+pub extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
+  main_result().unwrap();
+  0
+}
+
+fn main_result() -> Result<(), ()> {
+  const INPUT: &str = include_str!("../../inputs/day_2.csv");
+
+  let mut horizontal_position: usize = 0;
+  let mut depth: usize = 0;
+
+  for command_str in INPUT.lines() {
+    let (command, unit_str) = command_str.split_once(" ").unwrap_or_else(|| unreachable!("Missing command or unit"));
+    let unit = unit_str.parse::<usize>().map_err(|_| ())?;
+
+    match command {
+      "forward" => {
+        horizontal_position += unit;
+      }
+      "up" => {
+        depth -= unit;
+      }
+      "down" => {
+        depth += unit;
+      }
+      _ => unreachable!("Unknown command")
+    }
+  }
+
+  libc_println!("{} horizontal position and depth", horizontal_position * depth);
+
+  Ok(())
+}
